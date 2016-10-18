9daa2c29f38a26abb9e34911be58e0979e211e69
anran <370654630@qq.com>
2016-10-18 21:09:29 +0800

    day2.

---
+++ spider2.py
@@ -0,0 +1,32 @@
+from urllib import request
+import os
+import re
+
+target_path = "E:\photos2"
+def savefile(path):
+    if not os.path.isdir(target_path):
+        os.mkdir(target_path)
+
+    pos = path.rindex('/')
+# 字符串拼接
+    photo = os.path.join(target_path, path[pos+1:])
+    return photo
+
+
+if __name__ == '__main__':
+    url = "https://www.douban.com"
+
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/49.0.2623.75 Safari/537.36'}
+    req = request.Request(url, headers=header)
+
+    with request.urlopen(url) as res:
+        data = res.read()
+    s = set(re.findall(r'(https:[^s]*?(jpg|png|gif))', str(data)))  #re.findall()返回一个list
+    print(s)
+    for link, t in s:
+        print(link)
+        try:
+            request.urlretrieve(link, savefile(link))
+        except:
+            print('error')
